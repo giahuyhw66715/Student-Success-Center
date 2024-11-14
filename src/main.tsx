ac16a08b1f376a8491c77beb09005e8b7ec681d3
Harry <giahuyhw66715@gmail.com>
2024-11-14 13:05:55 +0700

    Reset app

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { NextUIProvider } from "@nextui-org/react";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import "swiper/css";
 // import "swiper/css/navigation";
@@ -19,12 +19,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <HashRouter>
+        <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <NextUIProvider>
                     <App />
                 </NextUIProvider>
             </QueryClientProvider>
-        </HashRouter>
+        </BrowserRouter>
     </ClerkProvider>
 );
